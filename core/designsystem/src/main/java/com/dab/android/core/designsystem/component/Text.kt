46df608ac2caf2559db757b112cab0769cdbf77a
WonSeokk <wwon.seokk@gmail.com>
2023-01-26 23:28:44 +0900

    fix: MainTitle Color Param 추가

--- core/designsystem/src/main/java/com/dab/android/core/designsystem/component/Text.kt
+++ core/designsystem/src/main/java/com/dab/android/core/designsystem/component/Text.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
@@ -11,10 +12,11 @@ import androidx.compose.ui.unit.dp
 import com.dab.android.core.designsystem.theme.DabTheme
 
 @Composable
-fun MainTitle(text: String, modifier: Modifier = Modifier) {
+fun MainTitle(text: String, modifier: Modifier = Modifier, color: Color = DabTheme.colors.surfaceOppositeColor) {
     Text(
         text = text,
         style = DabTheme.typography.titleLarge.copy(fontWeight = FontWeight.ExtraBold),
+        color = color,
         modifier = Modifier
             .padding(start = 8.dp, bottom = 5.dp, top = 5.dp)
             .semantics { heading() }
